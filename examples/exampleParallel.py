e66835727b69a7e4c3ed12ecdb30b8edde5c6533
Daniel Abrilot <daniel.abrilot@gmail.com>
2014-06-10 12:32:49 -0400

    arreglando cambios

--- examples/exampleParallel.py
+++ examples/exampleParallel.py
@@ -7,7 +7,7 @@ Created on 14-05-2014
 
 from mpi4py import MPI
 from tools.serial import get_pattern
-from tools.pdfstring import pdf2string
+from tools.pdftolist import pdf2string
 from time import time
 
 comm = MPI.COMM_WORLD()
@@ -21,7 +21,7 @@ if rank == master:
     print "leyendo la biblia . . ."
     
     path =  "./../files/biblia.pdf"
-    sheets = pdf2string(path, borrarCaracteresEspeciales=True, separadosPorHoja=True)
+    sheets = pdf2string(path)
     
     print "biblia leida en ", time() - t, " segundos"
 else:
