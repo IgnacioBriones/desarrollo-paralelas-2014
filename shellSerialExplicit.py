b07f19f976b19260adb925c5604167c5e62714fa
Daniel Abrilot <daniel.abrilot@gmail.com>
2014-06-18 19:46:55 -0400

    agregado nhojas

--- shellSerialExplicit.py
+++ shellSerialExplicit.py
@@ -4,7 +4,7 @@ Created on 14-05-2014
 
 @author: equipo de desarrollo
 '''
-from tools.serial import get_pattern, clearMatch
+from tools.serial import get_pattern
 from tools.pdftolist import pdf2string
 from tools.stringamatriz import str2matrix
 import json
@@ -12,7 +12,6 @@ import sys
 
 path, words = (sys.argv[1], sys.argv[2])
 words = words.split()
-words = words + [w[::-1] for w in words]
 sheets = pdf2string(path=path)
 
 rank = 0
@@ -28,16 +27,13 @@ rank = 0
 match = [[{'word':word, 'page':page, 'jump':rank + 1, 'position':get_pattern(text=sheet, rank=rank, word=word)}
       for page, sheet in enumerate(sheets)] for word in words ]
 
-# el match tiene todas las coincidencias de la biblia, pero dentro viene con campos vacios e informacion
-# incompleta que se debe deducir, como lo son por ejemplo, el numero de ocurrencias de una palabra en una hoja2
+match = sum(match, [])
+match = [m for m in match if m['position'] != set([])]
 
-match = clearMatch(match)
+for m in match:
+    m['position'] = list(m['position'])
 
 sheets = [str2matrix(text=sheet, ncol=60) for s in sheets]
-bible = {'sheets':sheets, 'match':match}
-print json.dumps(match)
-
-   
-
-
-    
+nhojas = [len(s) for s in sheets]
+bible = {'sheets':sheets, 'match':match, 'nhojas':nhojas}
+print json.dumps(bible)   
